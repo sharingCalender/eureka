054a8c2db29f7209cde8cee9904ba1b263cbd6d3
kimminjun <jj36282013@gmail.com>
2025-01-23 09:34:45 +0900

    init

---
+++ src/main/java/sharingcalender/eureka/security/SecurityConfig.java
@@ -0,0 +1,58 @@
+package sharingcalender.eureka.security;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+
+            .authorizeHttpRequests((auth) -> auth.anyRequest().authenticated())
+
+            .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+
+
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.builder()
+            .username(username)
+            .password(bCryptPasswordEncoder().encode(password))
+            .roles("ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(user1);
+    }
+}
